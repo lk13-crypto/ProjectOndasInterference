9936e22449f2e373a789dfaab9ef1534dd18b5fa
lk13-crypto <lkissola13@gmail.com>
2025-04-27 10:33:35 +0100

    Update ProjectOndas03ajustes.py
    O utilizador vai poder ajustar em tempo real as ondas no gráfico para obtenção do tipo de interferência que quiser nos intervalos definidos desses parâmetros. (não altere nenhuma linha ainda)Lk022

--- ProjectOndas03ajustes.py
+++ ProjectOndas03ajustes.py
@@ -20,8 +20,8 @@ ax.set_ylabel("Amplitude")
 
 x = np.linspace(0, 4 * np.pi, 1000)
 
-linha_onda1, = ax.plot([], [], label='Onda 1', color='blue')
-linha_onda2, = ax.plot([], [], label='Onda 2', color='green')
+linha_onda1, = ax.plot([], [], label ='Onda 1', color ='blue')
+linha_onda2, = ax.plot([], [], label ='Onda 2', color ='green')
 linha_resultante, = ax.plot([], [], label='Onda Resultante', color='red')
 
 ax.legend()
@@ -33,14 +33,14 @@ ax_amp2 = plt.axes([0.1, 0.15, 0.8, 0.03])
 ax_fase2 = plt.axes([0.1, 0.10, 0.8, 0.03])
 
 # Sliders
-slider_amp1 = Slider(ax_amp1, 'Amplitude 1', 0.1, 3.0, valinit=amplitude1_inicial)
+slider_ampli1 = Slider(ax_amp1, 'Amplitude 1', 0.1, 3.0, valinit=amplitude1_inicial)
 slider_fase1 = Slider(ax_fase1, 'Fase 1 (graus)', 0, 360, valinit=fase1_inicial)
 slider_amp2 = Slider(ax_amp2, 'Amplitude 2', 0.1, 3.0, valinit=amplitude2_inicial)
 slider_fase2 = Slider(ax_fase2, 'Fase 2 (graus)', 0, 360, valinit=fase2_inicial)
 
 # Função para determinar o tipo de interferência
 def tipo_interferencia(fase1, fase2):
-    diferenca_fase= abs(fase1 - fase2) % 360
+    diferenca_fase = abs(fase1 - fase2) % 360
     if diferenca_fase == 0 or diferenca_fase == 360:
         return "Construtiva"
     elif diferenca_fase == 180:
@@ -62,7 +62,7 @@ def animar(frame):
     fase1 = slider_fase1.val
     amp2 = slider_amp2.val
     fase2 = slider_fase2.val
-
+#calculo das amplitudes pela formula 
     y1 = amp1 * np.sin(x - t + np.radians(fase1))
     y2 = amp2 * np.sin(x - t + np.radians(fase2))
     y_total = y1 + y2
